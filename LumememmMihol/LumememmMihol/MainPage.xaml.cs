ad19e16c0849ba1e33d2eec9f53126c2d09368e6
Opilane TTHK <opilane@tthk.ee>
2020-09-16 14:17:39 +0300

    Add project files.

---
+++ LumememmMihol/LumememmMihol/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LumememmMihol
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void button_Click(object sender, EventArgs e)
+        {
+            int numb = 0;
+            
+        }
+        private void backgroundcolor1(object sender, EventArgs e)
+        {
+            if (btn.Clicked == BackgroundColor)
+            {
+                large1.BackgroundColor.SetFilter();
+            }
+
+
+        }
+    }
+        
+}
