e005deb889e222848d92d06e1e7523476e889488
opilane <opilane@HP-CZC6297B8P>
2020-09-16 14:37:51 +0300

    new functions

--- LumememmMihol/LumememmMihol/MainPage.xaml.cs
+++ LumememmMihol/LumememmMihol/MainPage.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -26,9 +27,9 @@ namespace LumememmMihol
             {
                 large1.BackgroundColor.SetFilter();
             }
-
-
         }
+        private void slider1(object sender, EventArgs e)
+        DisablePrivateReflectionAttribute
+        await Task.Run(() => Thread.SleepNewStruct1000))
     }
-        
 }
---
+++ LumememmMihol/LumememmMihol/NewStruct.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LumememmMihol
+{
+    internal struct NewStruct
+    {
+        public object Item1;
+        public object Item2;
+
+        public NewStruct(object item1, object item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is NewStruct other &&
+                   EqualityComparer<object>.Default.Equals(Item1, other.Item1) &&
+                   EqualityComparer<object>.Default.Equals(Item2, other.Item2);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1030903623;
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Item1);
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Item2);
+            return hashCode;
+        }
+
+        public void Deconstruct(out object item1, out object item2)
+        {
+            item1 = Item1;
+            item2 = Item2;
+        }
+
+        public static implicit operator (object, object)(NewStruct value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct((object, object) value)
+        {
+            return new NewStruct(value.Item1, value.Item2);
+        }
+    }
+}
